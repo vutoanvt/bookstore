ec6b6240ab4c1473c8bf7db20ba2a62c82b01e29
Toan <vutoanvt93@gmail.com>
2017-08-04 14:58:05 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Book Store</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/header.css">
+	<link rel="stylesheet" href="css/siderbar.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+
+<body>
+<div id="wrapper">
+	<!--/header/-->
+	<div id="header">
+		<!--/header_top/-->
+		<div id="header_top">
+			<div id="logo">
+				<h1>UIT Bookstore</h1>
+			</div>
+			<div id="cari">
+				<form>
+					<input id="timkiem" type="text" name="cari" size="30" placeholder="Nh?p thông tin c?n tìm...">
+					<button type="submit" class="button_1">Tìm ki?m</button>
+				</form>
+			</div>
+		</div>
+		<!--/header_center/-->
+		<div id="header_center">
+			<div id="menu_wrapper">
+				<ul id="menu">
+					<li><a href="index.php">Trang ch?</a></li>
+					<li><a href="#">Tin T?c</a></li>
+						<li class="dropdown">
+							<a href="index.html" class="dropbtn">T? sách</a>
+							<div class="dropdown-content">
+						      <a href="#">Van H?c - Ti?u Thuy?t</a>
+						      <a href="#">Khoa H?c - Công Ngh?</a>
+						      <a href="#">L?ch S? - Van Hóa</a>
+						      <a href="#">Tâm Lý - Giáo D?c</a>
+						      <a href="#">Ngo?i Ng?</a>
+						      <a href="#">Kinh T?</a>
+						      <a href="#">K? Nang S?ng</a>
+						    </div>
+						</li>
+					<li><a href="#">Tác gi?</a></li>
+					<li><a href="#">Gi?i thi?u</a></li>
+					<li><a href="#">Liên h?</a></li>
+				</ul>
+			</div>
+		</div>
+		<!--/header_bottom/-->
+		<div id="header_bottom">
+			<div id="slider">
+				<img src="images/banner1.jpg" alt="error" title="slider bohongan">
+			</div>
+			<div class="marquee">
+				<span id="font">* Chào m?ng các b?n dã d?n v?i th? gi?i phiêu luu c?a nh?ng trang sách *</span>
+			</div>
+		</div>
+	</div>
+	<!--/content/-->
+	<div id="content">
+		<!--/content_top/-->
+		<div id="content_top">
+			<h4>We will show you the way to success!</h4>
+			<p>It’s a quarter after seven. This phrase is one of the most common ways of stating this time. It means: “It’s 15 minutes past 7:00.” Another possibility here is to simply say: “It’s seven fifteen.” In general you can say: “It’s a quarter past the hour.” Have you ever thought about chucking it all and moving to a place where the greenback goes farther? Though many of us do, the spots we used to consider cheap—the Bahamas in the 1970s, Paris in the ’60s—aren’t so inexpensive now. Don’t worry. There are still plenty of places where the slightly well-heeled can live comfortably without giving up an arm and a leg. </p>
+		</div>
+		<!--/content_center/-->
+		<div id="content_center">
+			<div class="cc_news">
+				<h4>Sách bán ch?y</h4>
+				<img src="images/lichsu.jpg">
+					<a href="page.php">Thu Pháp Vi?t Nam</a> <br/>
+						Tác gi?: Nguy?n S? <br/>
+						D?ch gi?: <br/>
+						NXB liên k?t: Th? Gi?i <br/>
+						Giá bìa: 169.000 VND<br/>
+						S? trang: 285<br/>
+					<a href="#">Read more</a>
+			</div>
+			<div class="cc_news">
+				<h4>Sách M?i</h4>
+				<img src="images/chantroi.jpg">
+				<p>It’s a quarter after seven. This phrase is one of the most common ways of stating...
+				<a href="#">Read more</a>
+				</p>
+			</div>		
+			<div class="cc_news">
+				<h4>Sách Tâm Lý</h4>
+				<img src="images/hanoi.jpg">
+				<p>It’s a quarter after seven. This phrase is one of the most common ways of stating...
+				<a href="#">Read more</a>
+				</p>
+			</div>
+			<div class="cc_news">
+				<h4>Van H?c</h4>
+				<img src="images/dacula.jpg">
+				<p>It’s a quarter after seven. This phrase is one of the most common ways of stating...
+				<a href="#">Read more</a>
+				</p>
+			</div>		
+		</div>
+		
+		<!--/content_bottom/-->
+		<div id="content_bottom">
+			<div class="cb_news">
+				<h4>GI?I THI?U SÁCH M?I</h4>
+				<img src="images/slide5.jpg">
+				<p>It’s a quarter after seven. This phrase is one of the most common ways of stating this time. It means: “It’s 15 minutes past 7:00.”</p>
+				<ul>
+					<li><a href="#">Ti?n li?u có mua du?c h?nh phúc? (06/01/2017)</a></li>
+					<li><a href="#">Nguy?n Van Huyên và cu?c du hành trong 'Van minh Vi?t Nam' (05/01/2017)</a></li>
+					<li><a href="#">Vun d?p c?m xúc cho bé yêu ngay t? trong b?ng m? (05/01/2017)</a></li>
+					<li><a href="#">Cu?n ti?u thuy?t hi?n d?i d?u tiên c?a van h?c mi?n B?c (05/01/2017)</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<!-- Sidebar -->
+	<aside class="top-sidebar">
+		<article>
+			<h3>Sách bán ch?y trong tháng</h3>
+			<p>
+				<ul>
+					<li><a href="#">CÓ M?T PH? V?A ÐI QUA PH?</a></li>
+					<li><a href="#">M?T NGÀY C?A B?</a></li>
+					<li><a href="#">M?T MÌNH ? CHÂU ÂU</a></li>
+					<li><a href="#">CÔ GÁI NAM ?Y CHÚNG TA CÙNG...</a></li>
+					<li><a href="#">CON ÐU?NG STEVE JOBS</a></li>
+				</ul>
+			<p>You have the world available to you, and there are plenty of options for international exploration. Don’t worry. There are still plenty of places where the slightly well-heeled can live comfortably without giving up an arm and a leg.</p>
+		</article>
+	</aside>
+	<aside class="middle-sidebar">
+			<!--<h3>Ðang Nh?p</h3>
+			<form class="quote">
+				<div>
+					<p>Username</p>
+						<input type="text" placeholder="Email Address">
+				</div>
+				<div>
+					<p>Password</p>
+					<input type="Password" placeholder="*********">
+				</div>
+				<br>
+				<button class="button_1" type="submit">Ðang Nh?p</button>
+			</form>-->
+			<div class="row">
+				<div class="col-md-12">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4>Ðang nh?p</h4>
+						</div>
+						<div class="panel-body">
+							<form id="form-login" action="" method="POST" role="form">
+							<div id="errors">
+								</div>
+								<div class="form-group">
+									<label for="">Tên dang nh?p</label>
+									<input type="text" class="form-control" id="username" placeholder="Nh?p vào tên dang nh?p">
+								</div>
+								<div class="form-group">
+									<label for="">M?t kh?u</label>
+									<input type="text" class="form-control" id="password" placeholder="Nh?p vào m?t kh?u">
+								</div>
+								
+							
+								<button type="submit" class="btn btn-primary">Ðang nh?p</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+	</aside>
+</div>
+	<!--/footer/-->
+	<div id="footer">
+		<p>
+		<font color="#4ea6b7">Copyright</font> &copy;2017
+		All right reserved
+		</p>
+	</div>
+
+<script type='text/javascript'>window._sbzq||function(e){e._sbzq=[];var t=e._sbzq;t.push(["_setAccount",64483]);var n=e.location.protocol=="https:"?"https:":"http:";var r=document.createElement("script");r.type="text/javascript";r.async=true;r.src=n+"//static.subiz.com/public/js/loader.js";var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)}(window);
+</script>
+
+<!-- Latest compiled and minified CSS & JS -->
+
+<script src="//code.jquery.com/jquery.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" ></script>
+<script src="./js/login.js"></script>
+</body>
+</html>
---
+++ page.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Mua Sách</title>
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/header.css">
+		<link rel="stylesheet" href="css/siderbar.css">
+		<link rel="stylesheet" href="css/content.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	</head>
+	<body>
+		<div id="wrapper">
+<!--/header/-->
+			<div id="header">
+				<!--/header_top/-->
+				<div id="header_top">
+					<div id="logo">
+						<h1>UIT eBookstore</h1>
+					</div>
+					<div id="cari">
+						<form>
+							<input type="text" name="cari" size="30" placeholder="Nh?p thông tin c?n tìm..."/>
+							<button type="submit" class="button_1">Tìm ki?m</button>
+						</form>
+					</div>
+				</div>
+<!--/header_center/-->
+				<div id="header_center">
+					<div id="menu_wrapper">
+						<ul id="menu">
+							<li><a href="index.html">Trang ch?</a></li>
+							<li><a href="#">Tin T?c</a></li>
+							<li class="dropdown">
+								<a href="index.php" class="dropbtn">T? sách</a>
+								<div class="dropdown-content">
+									<a href="#">Van H?c - Ti?u Thuy?t</a>
+									<a href="#">Khoa H?c - Công Ngh?</a>
+									<a href="#">L?ch S? - Van Hóa</a>
+									<a href="#">Tâm Lý - Giáo D?c</a>
+									<a href="#">Ngo?i Ng?</a>
+									<a href="#">Kinh T?</a>
+									<a href="#">K? Nang S?ng</a>
+								</div>
+							</li>
+							<li><a href="#">Tác gi?</a></li>
+							<li><a href="#">Gi?i thi?u</a></li>
+							<li><a href="#">Liên h?</a></li>
+						</ul>
+					</div>
+				</div>
+				<!--/header_bottom/-->
+				<div id="header_bottom">
+					<div id="slider">
+						<img src="images/banner1.jpg" alt="error" title="slider bohongan">
+					</div>
+					<div class="marquee">
+						<span id="font">* Chào m?ng các b?n dã d?n v?i th? gi?i phiêu luu c?a nh?ng trang sách *</span>
+					</div>
+				</div>
+			</div>
+<!--/content/-->
+			<div id="content">
+				<!--/content_center/-->
+				<div class="content_page">
+					<h4>Sách Bán Ch?y</h4>
+						<img src="images/lichsu.jpg" align="left">
+							<h3 style="color:green">Thu Pháp Vi?t Nam</h3>
+							Tác gi?: Nguy?n S? <br/>
+							D?ch gi?: <br/>
+							NXB liên k?t: Th? Gi?i <br/>
+							Giá bìa: 169.000 VND<br/>
+							Ngày phát hành: 20-01-2017<br/>
+				</div>
+				
+				
+				<p>So lu?c v? tác ph?m</p>
+				<p>
+					“Krasznahorkai László - b?c th?y Hungary khi?n ngu?i ta nh? t?i Gogol và Melville tiên tri.”
+					- Susan Sontag
+					
+					Trong phòng luu tr? t?nh l? heo hút, cách Budapest tram hai muoi cây s?, Dr. Korin György tình c? phát hi?n m?t c?o b?n huy?n bí không nhan d?, không niên d?i, tác gi? vô danh. B?n ngu?i dàn ông trong c?o b?n, xu?t hi?n ? nh?ng bu?c ngo?t khác nhau c?a l?ch s? th? gi?i, lúc ? Creta, lúc ? Venice, lúc ? Köln, luôn ph?i tr?n ch?y kh?i chi?n tranh th?m kh?c, kh?i s? tàn phá. Và r?t lâu sau, trong m?t kho?nh kh?c tam t?i b?t ch?t vào sinh nh?t l?n th? b?n muoi tu, Korin c?m th?y chính mình cung c?n tr?n ch?y, nhu các nhân v?t trong t?p c?o b?n k? l? liên t?c tru?t v? phía tru?c, và cu?n gã vào mê l? c?a nó. Gã bán h?t gia s?n, d?n New York, d? k?t thúc cu?c d?i cu, và th?c hi?n gi?c mo dua c?o b?n quý giá vào cõi vinh h?ng...
+					
+				Chi?n tranh và chi?n tranh – ti?u thuy?t l?ng trong ti?u thuy?t, th? hi?n cái nhìn u t?i v? th?c t?i b?o tàn trong v? d?p bí ?n và bi th?m, nhung d?y s?c thôi miên, khi?n d?c gi? nh?p thân sâu s?c vào cu?c xê d?ch ác m?ng c?a nhân v?t di tìm ý nghia c?a th? gi?i và chính d?i mình. Chi?n tranh và chi?n tranh dã góp ph?n kh?ng d?nh v? trí c?a Krasznahorkai László nhu m?t tên tu?i l?n d?c bi?t dáng chú ý, m?t ngôi sao sáng khác l? trên b?u tr?i van h?c duong d?i Hungary và th? gi?i.</p>
+			</div>
+
+<!-- Sidebar -->
+			<aside class="top-sidebar">
+				<article>
+					<h3>Sách bán ch?y trong tháng</h3>
+					<p>
+						<ul>
+							<li><a href="#">CÓ M?T PH? V?A ÐI QUA PH?</a></li>
+							<li><a href="#">M?T NGÀY C?A B?</a></li>
+							<li><a href="#">M?T MÌNH ? CHÂU ÂU</a></li>
+							<li><a href="#">CÔ GÁI NAM ?Y CHÚNG TA CÙNG...</a></li>
+							<li><a href="#">CON ÐU?NG STEVE JOBS</a></li>
+						</ul>
+					</article>
+				</aside>
+
+				<aside class="middle-sidebar">
+					<article>
+						<h3>Ðang Nh?p</h3>
+						<form class="quote">
+							<div>
+								<p>Username</p>
+								<input type="text" placeholder="Email Address">
+							</div>
+							<div>
+								<p>Password</p>
+								<input type="Password" placeholder="*********">
+							</div>
+							<br>
+							<button class="button_1" type="submit">Ðang Nh?p</button>
+						</form>
+					</article>
+				</aside>
+
+<!--/footer/-->
+				<div id="footer">
+					<p>
+						<font color="#4ea6b7">Copyright</font> &copy;2017
+						All right reserved
+					</p>
+				</div>
+			</div>
+
+
+			<!-- Latest compiled and minified CSS & JS -->
+			
+			<script src="//code.jquery.com/jquery.js"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		</body>
+	</html>
